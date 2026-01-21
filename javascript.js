d012a33aa10ec9f3b3196e742bc37b11a198287b
FinianScott <25807449+FinianScott@users.noreply.github.com>
2026-01-21 14:05:52 -0500

    Add computer choice
    Code needs to select Rock, Paper or Scissors. Use mod functionality to
    select one at random.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-console.log("hello, world")
+function getComputerChoice(){
+    //SET num to random value from 0 to 9
+    let num = Math.floor(Math.random()*10);
+    //IF num is zero THEN
+    if (num === 0){
+        // return rerun of function (could theoretically run forever but not too worried)
+        return getComputerChoice();
+    //ELSE IF remainder of num and 3 is 0 THEN
+    } else if (num % 3 === 0 ){
+        // return Rock
+        return "Rock";
+    //ELSE IF remainder of num and 3 is 1 THEN
+    } else if (num % 3 === 1){
+        // return Paper
+        return "Paper";
+    //ELSE remainder of num and 3 is 2 THEN
+    } else {
+        // return Scissors
+        return "Scissors";
+    //ENDIF
+    }
+}
