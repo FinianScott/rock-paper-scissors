6595658c7ecb54f5a291dcc509ce9fff34c6f417
FinianScott <25807449+FinianScott@users.noreply.github.com>
2026-01-21 15:00:05 -0500

    Add round code
    Initial code for determining a round has been added. Will need to fine
    debug code for js.

--- javascript.js
+++ javascript.js
@@ -3,19 +3,19 @@ function getComputerChoice(){
     let num = Math.floor(Math.random()*10);
     //IF num is zero THEN
     if (num === 0){
-        // return rerun of function (could theoretically run forever but not too worried)
+        // RETURN rerun of function (could theoretically run forever but not too worried)
         return getComputerChoice();
     //ELSE IF remainder of num and 3 is 0 THEN
     } else if (num % 3 === 0 ){
-        // return Rock
+        // RETURN Rock
         return "Rock";
     //ELSE IF remainder of num and 3 is 1 THEN
     } else if (num % 3 === 1){
-        // return Paper
+        // RETURN Paper
         return "Paper";
     //ELSE remainder of num and 3 is 2 THEN
     } else {
-        // return Scissors
+        // RETURN Scissors
         return "Scissors";
     //ENDIF
     }
@@ -29,3 +29,43 @@ function getHumanChoice (){
 //INIT user and computer scores to 0
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    //SET humanChoice and computerChoice to uppercase
+    humanChoice = humanChoice.toUpperCase();
+    computerChoice = computerChoice.toUpperCase();
+
+    //IF humanChoice and computerChoice equal each other THEN
+        //RETURN it's a tie
+    if (humanChoice === computerChoice){
+        console.log("It's a tie!!!");
+        return;
+    }
+    //ELSE IF humanChoice and computer choice are (Rock, Scissors) OR (Paper, Rock) OR (Scissors, Paper) THEN
+        //RETURN human win.
+        //INCREMENT human score by one
+    else if ((humanChoice === "ROCK"&& computerChoice === "SCISSORS") || (humanChoice = "PAPER" && computerChoice=== "ROCK") || (humanChoice ==="SCISSORS"&& computerChoice === "PAPER")){
+        console.log(humanChoice);
+        console.log(`You win!!! ${humanChoice.charAt(0)}${humanChoice.slice(1).toLowerCase()} beats ${computerChoice.charAt(0)}${computerChoice.slice(1).toLowerCase()}!`);
+        humanScore = humanScore++;
+        return;
+        }
+    //ELSE THEN
+        //RETURN computer win 
+        //INCREMENT computer score by one
+    //END IF
+    else{
+        console.log(humanChoice);
+        console.log(`You lose!!! ${computerChoice.charAt(0)}${computerChoice.slice(1).toLowerCase()} beats ${humanChoice.charAt(0)}${humanChoice.slice(1).toLowerCase()}!`);
+        computerScore = computerScore++;
+        return;
+    }
+
+}
+
+//SET human and computer choices
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+// Use playRound function to determine winner. 
+playRound(humanSelection, computerSelection);
